3d0f9599688a07402f280ad231bf3e79ee8cdf6a
ganeshredcobra <ganeshredcobra@gmail.com>
2015-03-11 18:28:03 +0530

    Modified to merge multiple PDF's to single PDF

--- SCAN_DTS.sh
+++ SCAN_DTS.sh
@@ -21,7 +21,10 @@
 #       MA 02110-1301, USA.
 
 i=0
+START=0
 SCANDIR=~/Scanned_Documents/
+BOOL_MULTI=false
+PDFUNITE=pdfunite
 
 function SAV_CONV {
 	cd $SCANDIR
@@ -33,9 +36,27 @@ function SAV_CONV {
 	usb_modeswitch -R -v 04a9 -p 2759| tee >(zenity --progress  --text "Resetting" --pulsate --auto-close)
 	zenity --question --title="Question" --text="Do you want to scan again?"
 	if [[ $? == 0 ]] ; then
+		BOOL_MULTI=true
 		PROCESS
 	else
-   		nautilus $SCANDIR
+		if [ "$BOOL_MULTI" = true ] ;
+		then
+			zenity --question --title="Question" --text="Do you want to merge all PDF to One Page?"
+			if [[ $? == 0 ]] ; then
+				$PDFUNITE ${FIL_ARRAY[@]} final.pdf
+				if [[ $? == 0 ]] ; then
+					zenity --info --text "Merging PDF's done.Check for final.pdf."
+					nautilus $SCANDIR
+				else
+					zenity --info --text "Merging PDF's Failed."
+					nautilus $SCANDIR
+				fi					
+			else
+				nautilus $SCANDIR
+			fi	
+		else		
+	   		nautilus $SCANDIR
+		fi
 	fi	  
        }
 function EXIT {
@@ -43,6 +64,20 @@ function EXIT {
 		}
 function PROCESS {
 	ENTRY=$(zenity --entry --text "Please enter Document name" --title "Enter name")
+	if [ "$BOOL_MULTI" = true ] ;
+	then
+		#ENTRY_CNT={#FIL_ARRAY[@]}
+		NEW_CNT=$OLD_CNT+1
+		echo $NEW_CNT
+		echo "${#FIL_ARRAY[@]}"
+		FIL_ARRAY[$NEW_CNT]=$ENTRY.pdf 
+		OLD_CNT=$NEW_CNT
+		echo "${FIL_ARRAY[@]}"
+	else
+		FIL_ARRAY[START]=$ENTRY.pdf
+		echo ${FIL_ARRAY[START]}
+		OLD_CNT=$START		
+	fi
 	#scanimage| tee >(zenity --progress --title "TITLE" --text "SCANNING" --pulsate --auto-close) > $SCANDIR/$ENTRY.pnm
 	S_CHOICES=$(zenity --list --checklist --title="Output Type" --column="" --column="Export Format" FALSE "Grayscale" FALSE "Color");
 	#echo $S_CHOICES
@@ -71,5 +106,5 @@ function PROCESS {
 	fi
 		exit
 		}
-
+usb_modeswitch -R -v 04a9 -p 2759
 PROCESS

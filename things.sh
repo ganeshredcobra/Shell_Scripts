1aefff288af1a972734f08dc44014fc2fad00e7c
ganeshredcobra <ganeshredcobra@gmail.com>
2018-02-20 07:17:08 +0530

    Dynamically change Device ID in url and chnages in time

--- things.sh
+++ things.sh
@@ -22,7 +22,10 @@
 
 USER="renjith@beginow.in"
 PASSWD="renjith88"
-DEV1="5453e3b0-08ef-11e8-9141-1d8d2edf4f93"
+DEV1="d21f5600-0cd0-11e8-b10e-03e9461109ca"
+DEV2="5453e3b0-08ef-11e8-9141-1d8d2edf4f93"
+DEV3="3c69e6a0-1196-11e8-a6c4-03e9461109ca"
+ID=""
 declare -a KEYS=('Humidity' 'Temperature');
 
 
@@ -37,17 +40,19 @@ else
 		echo "Example: bash things.sh DEV1 2018-02-12 2018-02-18"
 	else
 		echo "Device ID:$1 Start Date:$2 End Date:$3" 
-		if [ $1 == "DEV1" ]; then
+		if [[ $1 == "DEV1"  || $1 == "DEV2"  || $1 == "DEV3" ]]; then
+			ID="${!1}"
+			echo $ID
 			date -d "$2" > /dev/null 2>&1
 			if [ $? -eq 0 ]; then
 			 # do something
 			 #echo "Return zero $2"
-			 SDATE=$(($(date -d "$2 + 1 days" +%s%N)/1000000))
+			 SDATE=$(($(date -d "$2 " +%s%N)/1000000))
 			 echo "SDATE is $SDATE"
 			 date -d "$3" > /dev/null 2>&1
 			 if [ $? -eq 0 ]; then
 				#echo "Return zero $3"
-				EDATE=$(($(date -d "$3 + 1 days" +%s%N)/1000000))
+				EDATE=$(($(date -d "$3 - 1 min" +%s%N)/1000000))
 				echo "EDATE is $EDATE"
 				JWT_TOKEN="$(curl -s -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"username":'\"$USER\"', "password":'\"$PASSWD\"'}' 'http://demo.thingsboard.io/api/auth/login'|jq -r '.token')"
 				#echo $JWT_TOKEN
@@ -55,7 +60,7 @@ else
 				for (( i=0; i<${#KEYS[@]}; i++ ));
 				do
 					#Made curl silent
-					curl -s -X GET "https://demo.thingsboard.io/api/plugins/telemetry/DEVICE/$DEV1/values/timeseries?keys=${KEYS[0]},${KEYS[1]}&startTs=$SDATE&endTs=$EDATE&interval=6000" --header "Content-Type:application/json" --header "X-Authorization: Bearer $JWT_TOKEN"|jq -r ".${KEYS[i]}|.[]|[(.ts|tostring),(.value|tostring)]| @csv" > ${KEYS[i]}.csv
+					curl -s -X GET "https://demo.thingsboard.io/api/plugins/telemetry/DEVICE/$ID/values/timeseries?keys=${KEYS[0]},${KEYS[1]}&startTs=$SDATE&endTs=$EDATE&interval=6000" --header "Content-Type:application/json" --header "X-Authorization: Bearer $JWT_TOKEN"|jq -r ".${KEYS[i]}|.[]|[(.ts|tostring),(.value|tostring)]| @csv" > ${KEYS[i]}.csv
 					echo "${KEYS[i]} exported to ${KEYS[i]}.csv"
 				done
 				#DATA="$(curl -v -X GET "https://demo.thingsboard.io/api/plugins/telemetry/DEVICE/$DEV1/values/timeseries?keys=${KEYS[0]},${KEYS[1]}&startTs=$SDATE&endTs=$EDATE&interval=6000" --header "Content-Type:application/json" --header "X-Authorization: Bearer $JWT_TOKEN"|jq -r '.Humidity|.[]|[(.ts|tostring),(.value|tostring)]| @csv' > humidity.csv)"
